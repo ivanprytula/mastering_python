4639747cc14a587e50351cd297c48dc21b42404c
ivanprytula <ivanprytula87@gmail.com>
2022-03-15 14:43:51 +0200

    add hierarchy for knowledge base/3 levels syllabus

--- quiz/knowledge_base/skip_for_flake.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-
-def wrong_func_name():
-    return 2 + 2
---
+++ quiz/quiz_brain.py
@@ -0,0 +1,6 @@
+class QuizBrain:
+    def __init__(self, questions):
+        # self.question_no = 0
+        # self.score = 0
+        self.questions = questions
+        # self.current_question = None
